2db9587afdb2d9d20b705fff1283517c453b5078
MDTrue <MDTrueII@hotmail.com>
2018-12-20 03:36:06 -0800

    skeleton pages done,lots of bugs

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,56 @@
+//data variables
+var friends = require("../data/friends.js")
+
+module.exports = function (app) {
+
+
+
+
+    //get route for json of possible friends
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    })
+
+    //post route for incoming survey results and compatibility logic
+
+    app.post("/api/friends", function (req, res) {
+
+        //placeholder variable & object
+        var totalDifference = 1000
+
+
+        var matchName = ""
+        var matchImg = ""
+
+
+        //users scores for comparison to friends
+        var userInput = req.body;
+        var userScores = userInput.scores
+
+
+
+        for (var j = 0; j < friends.length; j++) {
+            var difference = 0
+
+            for (var k = 0; k < userScores.length; k++) {
+                difference += Math.abs(friends[j], scores[k] - userScores[k]);
+            }
+
+            if (difference < totalDifference) {
+                totalDifference = difference;
+                bestMatch.name = friends[j].name
+                matchImage = friends[j].photo
+            }
+        }
+
+
+
+
+
+
+
+    })
+
+
+    
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+//requirements
+
+var express = require("express");
+var bodyParser = require("body-parser")
+var path = require("path")
+//setting app = express for shorthand
+var app = express();
+//connecting to localhost set up for heroku
+var PORT = process.env.PORT || 3000
+
+//bodyparser to access requests
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//Serve up static assets from public
+app.use(express.static(__dirname + "/public"));
+
+//mapping server to route files
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+
+
+//port listener set up
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT)
+  })
